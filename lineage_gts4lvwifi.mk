554671d1e985f4ceb642f72eae9530352fff6821
Rashed Abdel-Tawab <rashed@linux.com>
2019-07-07 11:02:30 -0700

    gts4lvwifi: Initial commit

---
+++ lineage_gts4lvwifi.mk
@@ -0,0 +1,41 @@
+#
+# Copyright (C) 2019 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+
+# Inherit from gts4lvwifi device
+$(call inherit-product, device/samsung/gts4lvwifi/device.mk)
+
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_tablet_wifionly.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := gts4lvwifi
+PRODUCT_NAME := lineage_gts4lvwifi
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := SM-T720
+PRODUCT_MANUFACTURER := samsung
+
+PRODUCT_GMS_CLIENTID_BASE := android-samsung
+
+# Use the latest approved GMS identifiers
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=gts4lvwifixx \
+    PRIVATE_BUILD_DESC="gts4lvwifixx-user 9 PPR1.180610.011 T720XXU1ASF1 release-keys"
+
+BUILD_FINGERPRINT := "samsung/gts4lvwifixx/gts4lvwifi:9/PPR1.180610.011/T720XXU1ASF1:user/release-keys"
