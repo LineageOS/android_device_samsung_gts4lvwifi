fcc762868e4b21e79b763ae53b91ccdc233eb657
Bruno Martins <bgcngm@gmail.com>
2020-11-05 17:41:42 +0000

    gts4lvwifi: Inherit from gts4lvwifi vendor
    Change-Id: Icfde20c7e72a187ee12a25df2e1f47a27a9ac485

--- device.mk
+++ device.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2019 The LineageOS Project
+# Copyright (C) 2019-2020 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,6 +13,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+
+# Get non-open-source specific aspects
+$(call inherit-product-if-exists, vendor/samsung/gts4lvwifi/gts4lvwifi-vendor.mk)
 
 # Local overlays
 DEVICE_PACKAGE_OVERLAYS += \
