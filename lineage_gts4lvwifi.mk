c4417a560135a49e2f6059297584066ea7739a88
LuK1337 <priv.luk@gmail.com>
2022-08-24 18:47:07 +0200

    gts4lvwifi: Update blobs from T720XXU2DVG3
    Change-Id: I01ebc9c82dea4df1a7ed6af48b98fd265863af3d

--- lineage_gts4lvwifi.mk
+++ lineage_gts4lvwifi.mk
@@ -38,6 +38,6 @@ PRODUCT_GMS_CLIENTID_BASE := android-samsung
 # Use the latest approved GMS identifiers
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_PRODUCT=gts4lvwifixx \
-    PRIVATE_BUILD_DESC="gts4lvwifixx-user 11 RP1A.200720.012 T720XXU2DUL1 release-keys"
+    PRIVATE_BUILD_DESC="gts4lvwifixx-user 11 RP1A.200720.012 T720XXU2DVG3 release-keys"
 
-BUILD_FINGERPRINT := "samsung/gts4lvwifixx/gts4lvwifi:11/RP1A.200720.012/T720XXU2DUL1:user/release-keys"
+BUILD_FINGERPRINT := "samsung/gts4lvwifixx/gts4lvwifi:11/RP1A.200720.012/T720XXU2DVG3:user/release-keys"
